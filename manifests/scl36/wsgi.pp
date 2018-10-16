ee61a4f56c3bff7e91db01deee34182cfdeb47a8
mh <mh@immerda.ch>
2018-10-16 20:15:32 +0200

    correct var namin and refactor pkg ordering

--- manifests/scl36/wsgi.pp
+++ manifests/scl36/wsgi.pp
@@ -1,13 +1,12 @@
 class python::scl36::wsgi(
-  $wsg_pkg_name = 'rh-python36-mod_wsgi-system-httpd'
+  $wsgi_pkg_name = 'rh-python36-mod_wsgi-system-httpd'
 ) {
   require ::scl
-  package{$wsg_pkg_name:
-    ensure  => 'installed',
-    require => Package['apache', 'rh-python36-python-virtualenv'],
-    notify  => Service['apache'],
-  }
   package{"rh-python36-python-virtualenv":
     ensure => 'installed',
+  } -> package{$wsgi_pkg_name:
+    ensure  => 'installed',
+    require => Package['apache'],
+    notify  => Service['apache'],
   }
-} 
+}
